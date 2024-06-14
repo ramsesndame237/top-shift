78d305608c50ce42adf874ba8dc344990f095b15
Ramses ndame <64875585+ramsesndame237@users.noreply.github.com>
2024-06-14 03:09:02 +0100

    Feat : animate and create categorie buttons
    i get the data categories to the server create a button to display it and animate that button
    Reviewed-by: Ramses ndame <64875585+ramsesndame237@users.noreply.github.com>

--- src/app/[locale]/(pages)/components/best-dispensary.tsx
+++ src/app/[locale]/(pages)/components/best-dispensary.tsx
@@ -3,27 +3,68 @@ import {useTranslation} from "react-i18next";
 import Link from "next/link";
 import {useCategory} from "@/app/[locale]/(pages)/hooks/categoryHooks";
 import {Categories} from "@/types/app.types";
+import {useState} from "react";
+import {AnimatePresence, motion} from "framer-motion";
+import {Carousel, CarouselContent, CarouselItem} from "@/components/ui/carousel";
 
 export const BestDispensary = () => {
     const {t} = useTranslation('common');
 
-   const  {data:categories,isLoading} = useCategory()
+    const {data: categories, isLoading} = useCategory()
 
+    const [isSelected, setIsSelected] = useState<string | undefined>(undefined)
     return (
         <div>
             <h3 className="text-3xl text-center">
                 {t('translate_key_dispensary_title')}
             </h3>
-            <div className="border border-red-500 ">
-                {isLoading ? <div>Loading...</div> :
-                 categories?.map((category:string,index:number) => (
-                     <button key={index} className="border-1 border-gray-400 h-10">
-                         {category}
-                     </button>
-                 ))
-                }
-
-            </div>
+            <Carousel>
+                <CarouselContent>
+                    {isLoading ? <div>Loading...</div> :
+                        categories?.map((category: string, index: number) => (
+                            <CarouselItem className="basis-1/2 mx-3">
+                                <AnimatePresence key={index} mode="wait">
+                                    {isSelected === category ? (
+                                        <motion.button
+                                            className="relative flex h-10  min-w-32  items-center w-fit text-nowrap  justify-center overflow-hidden rounded-full  px-5 py-2.5 bg-primary"
+                                            onClick={() => setIsSelected(undefined)}
+                                            initial={{opacity: 0}}
+                                            animate={{opacity: 1}}
+                                            exit={{opacity: 0}}
+                                        >
+                                            <motion.span
+                                                key="action"
+                                                className="relative block h-full w-full text-sm text-white font-bold"
+                                                initial={{y: -50}}
+                                                animate={{y: 0}}
+                                            >
+                                                {category.charAt(0).toUpperCase() + category.slice(1)}
+                                            </motion.span>
+                                        </motion.button>
+                                    ) : (
+                                        <motion.button
+                                            className="relative flex min-w-40 h-10 cursor-pointer text-nowrap w-fit items-center overflow-hidden text-ellipsis whitespace-nowrap justify-center rounded-full border border-b-gray-300  px-5 py-2.5"
+                                            onClick={() => setIsSelected(category)}
+                                            initial={{opacity: 0}}
+                                            animate={{opacity: 1}}
+                                            exit={{opacity: 0}}
+                                        >
+                                            <motion.span
+                                                key="reaction"
+                                                className="relative block text-sm font-bold"
+                                                initial={{x: 0}}
+                                                exit={{x: 50, transition: {duration: 0.1}}}
+                                            >
+                                                {category.charAt(0).toUpperCase() + category.slice(1)}
+                                            </motion.span>
+                                        </motion.button>
+                                    )}
+                                </AnimatePresence>
+                            </CarouselItem>
+                        ))
+                    }
+                </CarouselContent>
+            </Carousel>
 
             <div
                 className="bg-primary w-full min-h-96 relative flex items-center justify-center flex-col rounded-2xl overflow-hidden">
@@ -38,7 +79,8 @@ export const BestDispensary = () => {
                         {t('translate_key_shop_best_title')}
                     </h4>
                     <p className="text-md opacity-60 max-w-[216px] text-center">
-                        Lorem ipsum dolor sit amet consectetur. Ullamcorper ipsum varius lorem blandit lectus magnis feugiat.
+                        Lorem ipsum dolor sit amet consectetur. Ullamcorper ipsum varius lorem blandit lectus magnis
+                        feugiat.
                     </p>
                     <Link href="#" className="text-secondary underline mt-2">{t('translate_key_view_all')}</Link>
                 </div>
--- src/providers/TanstackProvider.tsx
+++ src/providers/TanstackProvider.tsx
@@ -4,7 +4,6 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import React from "react";
-
 const TanstackProvider = ({children}:{children:React.ReactNode})=> {
 
     const queryClient = new QueryClient({
